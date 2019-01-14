473f508c1ce6ae03a568ee1d90fd6ed3f9b25789
Klarmeister <pfilibb@gmail.com>
2019-01-14 16:16:51 +0100

    Kreis und Linienerkennung; Opencv
    Kreiserkennung und Linienerkennung testweise implementiert
    OpenCV hinzugefuegt

--- OpenCVLiveLoopVC12/OpenCVLiveLoopVC12/OpenCVLiveLoopVC12/OpenCVLiveLoopVC12.cpp
+++ OpenCVLiveLoopVC12/OpenCVLiveLoopVC12/OpenCVLiveLoopVC12/OpenCVLiveLoopVC12.cpp
@@ -115,7 +115,50 @@ int MonoLoop()
     }
 
     /*******************************todo*****************************/
-    outputFrame = inputFrame;
+	cv::Mat src_gray, src;
+	cv::Mat dst, cdst;
+
+	src = inputFrame;
+	cvtColor(inputFrame, src_gray, CV_BGR2GRAY);
+
+	//lines1
+	Canny(src, dst, 50, 200, 3);
+	cvtColor(dst, cdst, CV_GRAY2BGR);
+	vector<cv::Vec2f> lines;
+	HoughLines(dst, lines, 1, CV_PI / 180, 100, 0, 0);
+	for (size_t i = 0; i < lines.size(); i++)
+	{
+		float rho = lines[i][0], theta = lines[i][1];
+		cv::Point pt1, pt2;
+		double a = cos(theta), b = sin(theta);
+		double x0 = a * rho, y0 = b * rho;
+		pt1.x = cvRound(x0 + 1000 * (-b));
+		pt1.y = cvRound(y0 + 1000 * (a));
+		pt2.x = cvRound(x0 - 1000 * (-b));
+		pt2.y = cvRound(y0 - 1000 * (a));
+		line(src, pt1, pt2, cv::Scalar(0, 0, 255), 3, CV_AA);
+	}
+
+	/// Reduce the noise so we avoid false circle detection
+	GaussianBlur(src_gray, src_gray, cv::Size(9, 9), 2, 2);
+
+	vector<cv::Vec3f> circles;
+
+	/// Apply the Hough Transform to find the circles
+	HoughCircles(src_gray, circles, CV_HOUGH_GRADIENT, 1, 100, 10, 90, 0, 0);
+
+	/// Draw the circles detected
+	for (size_t i = 0; i < circles.size(); i++)
+	{
+		cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+		int radius = cvRound(circles[i][2]);
+		// circle center
+		circle(src, center, 3, cv::Scalar(0, 255, 0), -1, 8, 0);
+		// circle outline
+		circle(src, center, radius, cv::Scalar(0, 0, 255), 3, 8, 0);
+	}
+
+    outputFrame = src;
     /***************************end todo*****************************/
     
     imshow("cam", outputFrame);
@@ -217,4 +260,5 @@ int _tmain(int argc, _TCHAR* argv[])
 
   return MonoLoop();
 }
+
 
